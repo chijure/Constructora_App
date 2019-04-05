55c3a8e6017fb6598e57d0536bd931945b66e5e1
Victor Torres <vatorresm88@gmail.com>
2019-04-05 12:27:25 -0500

    Actualizando los links de la vista intranet

--- constructora/app/controllers/administration_controller.rb
+++ constructora/app/controllers/administration_controller.rb
@@ -1,16 +1,25 @@
 class AdministrationController < ApplicationController
   def base_price
+    render "pages/administration/base_price"
   end
 
   def report
+    render "pages/administration/report"
   end
 
   def proforma
+    render "pages/administration/proforma"
   end
 
   def register_sales
+    render "pages/administration/register_sales"
   end
 
   def separate_departament
+    render "pages/administration/separate_departament"
+  end
+
+  def intranet
+    render "pages/administration/intranet"
   end
 end
