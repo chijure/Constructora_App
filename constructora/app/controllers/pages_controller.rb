134c447d83ec36df0cddf48773ff0455d2bac443
Cristhian <cbazanl@outlook.com>
2019-04-06 10:40:40 -0500

    vistas de About us y Contacto web
    vistas de About us y Contacto web

--- constructora/app/controllers/pages_controller.rb
+++ constructora/app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
 class PagesController < ApplicationController
   def index
   end
+
+  def aboutus
+  end
+  
+  def contactus
+  end
+
 end
--- constructora/config/routes.rb
+++ constructora/config/routes.rb
@@ -31,6 +31,8 @@ Rails.application.routes.draw do
   get 'project/edit'
   get 'project/details'
 
+  get 'pages/aboutus'
+  get 'pages/contactus'
   root 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
